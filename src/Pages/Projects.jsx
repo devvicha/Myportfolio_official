2d33acc40f3135d26e7105c28af701cc2cbde769
devvicha <160200393+devvicha@users.noreply.github.com>
2025-08-23 21:42:08 +0530

    host1

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -205,7 +205,7 @@ const Projects = () => {
           viewport={{ once: false, amount: 0.2 }}
         >
           <a
-            href="https://github.com/chamithsandeepa"
+            href="https://github.com/devvicha"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-flex items-center bg-purple-600 hover:bg-purple-700 text-white py-3 px-6 rounded-full transition-all duration-300 text-lg font-medium"
